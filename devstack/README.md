ecc392f5545e272d4be8969f1d92d4dda0ec1210
Chandra Mohan Babu Nadiminti <cnadimin@cisco.com>
2016-02-25 14:17:49 -0800

    Simple Devstack setup

---
+++ devstack/README.md
@@ -0,0 +1,9 @@
+# devstack
+
+Install OpenStack for development purpose using [devstack](http://docs.openstack.org/developer/devstack)
+
+Run `vagrant up`, wait for the complete installation (go get some coffee) and login at http://localhost:8080/dashboard
+Credentials: `admin` or `demo`, `password`
+
+Explore more inside the `local.conf` file
+
---
+++ devstack/bootstrap.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+set -e
+
+p_dir=`dirname $0`
+
+function run_unstack {
+    if [ -d ${p_dir}/devstack ]; then
+        echo '--------------- Running unstack.sh -----------'
+        ${p_dir}/devstack/unstack.sh
+        echo '--------------- Running clean.sh -----------'
+        ${p_dir}/devstack/clean.sh
+    fi
+}
+
+function clone_devstack {
+    echo '--------------- Cloning fresh devstack -----------'
+    if [ -d ${p_dir}/devstack ]; then
+        rm -rf ${p_dir}/devstack
+    fi
+    git clone https://github.com/openstack-dev/devstack -b stable/liberty ${p_dir}/devstack
+}
+
+function run_stack {
+    echo '--------------- Running stack.sh -----------'
+    cp ${p_dir}/local.conf ${p_dir}/devstack/local.conf
+    ${p_dir}/devstack/stack.sh
+}
+
+run_unstack || true
+clone_devstack
+run_stack
+
